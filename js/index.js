e67b56979a46f2ce193ea6c8098dcaba879e3d20
Daniil <d.kirykha2010@gmail.com>
2023-06-07 14:49:34 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const age = 13;
+console.log(age)
+const name = "Daniil";
+console.log(name)
+const isStudent = true;
+console.log(isStudent)
+const isString = "Безвихідних ситуацій впринципі не буває.";
+console.log(isString)
+let myNumber = 20;
+myNumber = 20+10;
+console.log(myNumber)
+const myNull = null;
+console.log(myNull)
+const nameUser = prompt("What's your name?")
+console.log('You log in user name=>', nameUser)
+const windConfirm = confirm('Хочете вийти?');
+if (windConfirm === true){
+    alert("Дякую за підтвердження!")
+}else{
+    alert("Дію відмінено!")
+}
+alert("Дія небезпечна")
+const windowConfirm = confirm('Хочете завантажити?');
+if (windowConfirm === true){
+    alert("Дякую за підтвердження!")
+}else{
+    alert("Дію відмінено!")
+}
+
+
+
+
